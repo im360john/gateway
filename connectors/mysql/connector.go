e71cf4c4309dd616239d076d0fb2809f3fa43dd4
tserakhau <laskoviymishka@gmail.com>
2025-02-20 13:28:13 +0100

    add mysql connector implementation

---
+++ connectors/mysql/connector.go
@@ -0,0 +1,147 @@
+package mysql
+
+import (
+	"context"
+	"fmt"
+	"github.com/centralmind/gateway/connectors"
+	"github.com/centralmind/gateway/model"
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+)
+
+func init() {
+	connectors.Register[Config](func(cfg Config) (connectors.Connector, error) {
+		dsn, err := cfg.MakeDSN()
+		if err != nil {
+			return nil, errors.Errorf("unable to prepare mysql config: %w", err)
+		}
+		db, err := sqlx.Open("mysql", dsn)
+		if err != nil {
+			return nil, errors.Errorf("unable to open mysql db: %w", err)
+		}
+		return &Connector{
+			config: cfg,
+			db:     db,
+		}, nil
+	})
+}
+
+type Config struct {
+	Host      string
+	Database  string
+	User      string
+	Password  string
+	Port      int
+	TLSConfig string
+}
+
+func (c Config) MakeDSN() (string, error) {
+	cfg := mysql.Config{
+		User:                 c.User,
+		Passwd:               c.Password,
+		Net:                  "tcp",
+		Addr:                 fmt.Sprintf("%s:%d", c.Host, c.Port),
+		DBName:               c.Database,
+		AllowNativePasswords: true,
+		ParseTime:            true,
+		TLSConfig:            c.TLSConfig,
+	}
+	return cfg.FormatDSN(), nil
+}
+
+func (c Config) Type() string {
+	return "mysql"
+}
+
+type Connector struct {
+	config Config
+	db     *sqlx.DB
+}
+
+func (c Connector) Sample(ctx context.Context, table model.Table) ([]map[string]any, error) {
+	rows, err := c.db.QueryxContext(ctx, fmt.Sprintf("SELECT * FROM %s LIMIT 5", table.Name))
+	if err != nil {
+		return nil, errors.Errorf("unable to query db: %w", err)
+	}
+	defer rows.Close()
+
+	res := make([]map[string]any, 0, 5)
+	for rows.Next() {
+		row := make(map[string]any)
+		if err := rows.MapScan(row); err != nil {
+			return nil, errors.Errorf("unable to scan row: %w", err)
+		}
+		res = append(res, row)
+	}
+	return res, nil
+}
+
+func (c Connector) Discovery(ctx context.Context) ([]model.Table, error) {
+	rows, err := c.db.Query("SELECT table_name FROM information_schema.tables WHERE table_schema = ?", c.config.Database)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var tables []model.Table
+	for rows.Next() {
+		var tableName string
+		if err := rows.Scan(&tableName); err != nil {
+			return nil, err
+		}
+		columns, err := c.LoadsColumns(ctx, tableName)
+		if err != nil {
+			return nil, err
+		}
+		tables = append(tables, model.Table{Name: tableName, Columns: columns})
+	}
+	return tables, nil
+}
+
+func (c Connector) Ping(ctx context.Context) error {
+	return c.db.PingContext(ctx)
+}
+
+func (c Connector) Query(ctx context.Context, endpoint model.Endpoint, params map[string]any) ([]map[string]any, error) {
+	rows, err := c.db.NamedQueryContext(ctx, endpoint.Query, params)
+	if err != nil {
+		return nil, errors.Errorf("unable to query db: %w", err)
+	}
+	defer rows.Close()
+
+	res := make([]map[string]any, 0)
+	for rows.Next() {
+		row := map[string]any{}
+		if err := rows.MapScan(row); err != nil {
+			return nil, errors.Errorf("unable to scan row: %w", err)
+		}
+		res = append(res, row)
+	}
+	return res, nil
+}
+
+func (c Connector) LoadsColumns(ctx context.Context, tableName string) ([]model.ColumnSchema, error) {
+	rows, err := c.db.QueryContext(
+		ctx,
+		"SELECT COLUMN_NAME, DATA_TYPE FROM information_schema.columns WHERE table_name = ? AND table_schema = ?",
+		tableName, c.config.Database,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var columns []model.ColumnSchema
+	for rows.Next() {
+		var name, dataType string
+		if err := rows.Scan(&name, &dataType); err != nil {
+			return nil, err
+		}
+		columns = append(columns, model.ColumnSchema{
+			Name: name,
+			Type: dataType,
+		})
+	}
+	return columns, nil
+}
